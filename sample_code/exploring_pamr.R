74d60e8e3590a0bd2f2d45e9a23da6cc2e9c527c
Lorenzo_Lazzari <lorenzo.lazzari98@gmail.com>
2023-03-10 15:51:27 +0100

    continued to explore pamr package

--- sample_code/exploring_pamr.R
+++ sample_code/exploring_pamr.R
@@ -1,13 +1,4 @@
-#SRBCT dataset from library genomic
-library(plsgenomics)
-data(SRBCT)
-?SRBCT
-str(SRBCT) #already ready for pamr, only renames X->x, Y->y
-SRBCT$x=t(SRBCT$X) #pamr wants genes(rows)xsamples(columns)
-SRBCT$y=SRBCT$Y
-str(SRBCT)
-
-#SRBCT dataset from the library pamr it's self
+# SRBCT dataset from the library pamr it's self ####
 data(khan) #used in tibs 2002 PNAS
 str(khan) #still needs preprocessing
 length(khan)
@@ -24,7 +15,7 @@ str(khanc$x) #now ok
 
 khanc$y=as.factor(as.matrix(khan[1,3:ncol(khan)]))
 str(khanc$y)
-#khanc$geneid=khan[,2]
+khanc$geneid=as.character(as.matrix(khan[,1]))
 str(khanc) #63obs like in tibs 2002
 
 
@@ -46,10 +37,62 @@ thresh=pamr.adaptthresh(pamrtrain) #producing thresholds scale different for cla
 pamrtrain.adapt=pamr.train(khanc, threshold.scale = thresh )
 pamrtrain.adapt
 
-# pamr.confusion ####
+# confusion ####
 ?pamr.confusion()
 pamr.confusion(pamrtrain, threshold = 7.07)
 pamrtrain$sample.subset
 
 
+
+# cross-validation ####
+cvpamr=pamr.cv(pamrtrain, khanc)
+cvpamr$folds
+cvpamr$cv.objects[[1]] #if want to add pamr.cv as input of vcr.pamr shoulkd understand this object
+# geneplot ####
+pamr.geneplot(pamrtrain,khanc,threshold=7)
+# indeterminate ####
+pamr.indeterminate(pamrtrain$prob, mingap=0.2) #should use pamr predict
+# listgenes ####
+pamr.listgenes(pamrtrain, khanc, 4.2)
+# makeclass ####
+pamr.makeclasses(khanc)
+# menu ####
+
+
+# plots ####
+par(mar = c(6, 6, 6, 6) + 0.1)
+pamr.plotcen(pamrtrain, khanc, 6)
+pamr.plotcv(cvpamr)
+dev.off()
+pamr.plotcvprob(cvpamr,khanc,4) #the visualization we are trying to improve
+pamr.plotfdr()
+#
+# predict ####
+pamr.predict(pamrfit)
+
+
+# pamr with survival data ####
+gendata<-function(n=100, p=2000){
+  tim <- 3*abs(rnorm(n))
+  u<-runif(n,min(tim),max(tim))
+  y<-pmin(tim,u)
+  ic<-1*(tim<u)
+  m <- median(tim)
+  x<-matrix(rnorm(p*n),ncol=n)
+  x[1:100, tim>m] <- x[1:100, tim>m]+3
+  return(list(x=x,y=y,ic=ic))
+}
+# generate training data; 2000 genes, 100 samples
+junk<-gendata(n=100)
+y<-junk$y
+ic<-junk$ic
+x<-junk$x
+d <- list(x=x,survival.time=y, censoring.status=ic,
+          geneid=as.character(1:nrow(x)), genenames=paste("g",as.character(1:nrow(x)),sep=
+                                                            ""))
+# train model
+a3<- pamr.train(d, ngroup.survival=4)
+a3
+pamr.plotstrata(a3, d$survival.time, d$censoring.status)
+pamr.confusion.survival(a3, d$survival.time, d$censoring.status, a3$yhat[[3]])
 
