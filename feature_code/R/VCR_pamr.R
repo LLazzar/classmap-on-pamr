e0507cedf320ed611140dc107d6b2d33814b94a6
Lorenzo_Lazzari <lorenzo.lazzari98@gmail.com>
2023-05-17 19:42:49 +0200

    review of previoyus code and started adding a draft of vcr.pamr.newdata

--- feature_code/R/VCR_pamr.R
+++ feature_code/R/VCR_pamr.R
@@ -53,7 +53,6 @@ vcr.pamr.train <- function(data, pamrfit, pamrfitcv=NULL, threshold) {
   #   ofarness  : For each object i, its lowest farness to any
   #               class, including its own. Always exists.
   #
-  keepPCA <- TRUE; prec <- 1e-10  ####PROBABLY TO BE REMOVED
   #
   # Subsetting to the same subset (of variables and observation) on which pamr fit works on.
 
@@ -78,7 +77,7 @@ vcr.pamr.train <- function(data, pamrfit, pamrfitcv=NULL, threshold) {
   d <- ncol(X)
   if (n < 2) stop("The training data should have more than one case.")
 
-  y=as.factor(data$y) #factorize the given classes
+  y=as.factor(data$y) #FACTORIZE IT OR NOT?
 
   # Check whether y and its levels are of the right form:
   checked <- checkLabels(y, n, training = TRUE) #PROBABLY SHOULD RE DIG DEEP TO UNDERSTAND THIS FUNCTION
@@ -90,9 +89,8 @@ vcr.pamr.train <- function(data, pamrfit, pamrfitcv=NULL, threshold) {
   nlab    <- length(levels)
   yint    <- lab2int(y) #given label (true) as integer
   yintv   <- yint[indsv]
-  classSizes <- rep(0, nlab)  ###PROBABLY TO BE REMOVED
-  for (g in seq_len(nlab)) {classSizes[g] <- sum(yintv == g)} ### PROBABLY TO BE REMOVED
-  # classSizes
+  #
+  #
   #
   # Getting threshold index from inputted threshold value (idea of this code/logic from pamr.confusion)
   #
@@ -298,10 +296,6 @@ vcr.pamr.train <- function(data, pamrfit, pamrfitcv=NULL, threshold) {
 
   figparams <- farout$figparams
   figparams$ncolX <- d
-  #figparams$computeMD <- computeMD check what is that, probabbly related only to neuralnet
-  #figparams$classMS <- classMS check what is that, probbably related to neuralnet
-  #figparams$PCAfits <- farout$PCAfits #only for neuralnet??
-
 
   ####################################################
   # calculating pairwise distances, for additional visualization feature
@@ -345,8 +339,233 @@ vcr.pamr.train <- function(data, pamrfit, pamrfitcv=NULL, threshold) {
               fig = farout$fig,
               farness = farout$farness,
               ofarness = farout$ofarnes,
+              pamrfit = pamrfit, #needed for computations in vcr.pamr.newdata
+              pamrfitcv = pamrfitcv, #needed to test in vcr.pamr.newdata whether vcr.pamr.train out is right
+              threshold = threshold,
               initfig=initfig, #INITFIG ADDED FOR TEST
               posid=posid, #added for test
               sd=sd#pwd=pwd #pairwise matrix distance for mds
               ))
 }
+
+vcr.pamr.newdata <- function(newdata, vcr.pamr.train.out, threshold=vcr.pamr.train.out$threshold, prior=NULL, threshold.scale=NULL){
+  #
+  # Prepares graphical display of new data fitted by a neural
+  # net that was modeled on the training data, using the output
+  # of vcr.pamr.train() on the training data.
+  #
+  # Arguments:
+  #   Xnew                 : data matrix of the new data, with the
+  #                          same number of columns d as in the
+  #                          training data.
+  #                          Missing values are not allowed.
+  #   ynew                 : factor with class membership of each new
+  #                          case. Can be NA for some or all cases.
+  #                          If NULL, is assumed to be NA everywhere.
+  #   probs                : posterior probabilities obtained by
+  #                          running the neural net on the new data.
+  #   vcr.neural.train.out : output of vcr.neural.train() on the
+  #                          training data.
+  #
+  # Returns:
+  #   yintnew   : given labels as integers 1, 2, 3, ..., nlab.
+  #               Can have NA's.
+  #   ynew      : labels if yintnew is available, else NA.
+  #   levels    : levels of the response, from vcr.neural.train.out
+  #   predint   : predicted label as integer, always exists.
+  #   pred      : predicted label of each object
+  #   altint    : alternative label as integer if yintnew was given,
+  #               else NA.
+  #   altlab    : alternative label if yintnew was given, else NA.
+  #   classMS   : list with center and covariance matrix of each class,
+  #               from vcr.neural.train.out
+  #   PAC       : probability of alternative class for each object
+  #               with non-NA yintnew.
+  #   figparams : (from training) parameters used to compute fig
+  #   fig       : farness of each object i from each class g.
+  #               Always exists.
+  #   farness   : farness of each object to its given class, for
+  #               objects with non-NA yintnew.
+  #   ofarness  : For each object i, its lowest farness to any
+  #               class, including its own. Always exists.
+  #
+  # subsetting to same subset of gene
+  newdata$x=newdata$x[pamrfit$gene.subset,pamrfit$sample.subset]
+
+  Xnew <- as.matrix(t(newdata$x))
+
+  if (nrow(Xnew) == 1) Xnew <- t(Xnew)
+  n <- nrow(Xnew)
+  d <- vcr.pamr.train.out$figparams$ncolX
+  if (ncol(Xnew) != d) {
+    stop(paste0("newdata$x should have ", d,
+                " columns, like the training data$x."))
+  }
+  if (sum(is.na(as.vector(Xnew))) > 0) {
+    stop("The coordinate matrix newdata$x contains NA's.")
+  }
+  levels <- vcr.pamr.train.out$levels # as in training data
+  nlab   <- length(levels) # number of classes
+
+  ynew=newdata$y
+
+  if (is.null(ynew)) ynew <- factor(rep(NA, n), levels = levels)
+  checked <- checkLabels(ynew, n, training = FALSE, levels)
+  lab2int <- checked$lab2int # is a function
+  indsv   <- checked$indsv   # INDiceS of cases we can Visualize,
+  #                         # can even be empty, is OK.
+  nvis    <- length(indsv)   # can be zero.
+  yintnew <- rep(NA, n)       # needed to overwrite "new" levels.
+  yintnew[indsv] <- lab2int(ynew[indsv])
+  #
+  #
+  # compute posteriors using pamr.predict (should import) #########
+
+  if (length(dim(probs)) != 2) stop("probs should be a matrix.")
+  if (ncol(probs) == 1) probs <- t(probs) # if new data is 1 object
+  if (ncol(probs) != nlab) stop(paste0(
+    "The matrix probs should have ", nlab, " columns"))
+  if (any(is.na(probs))) stop("probs should not have any NA's.")
+  #
+  # Compute prediction for all objects in the new data:
+  #
+  predint <- apply(probs[, , drop = FALSE], 1, which.max)
+  #
+  # Compute PAC for all objects with available y:
+  #
+  ptrue <- palt <- altint <- PAC <- rep(NA, n)
+  if (nvis > 0) { # if there are new data with labels
+    yintv  <- yintnew[indsv] # yint of cases we will Visualize
+    ayintv <- sort(unique(yintv)) # available yintv values
+    # ayintv can be a proper subset of 1, ..., nlab for new data.
+    for (g in ayintv) {
+      clinds <- indsv[which(yintv == g)] # indices in 1, ..., n
+      others <- (seq_len(nlab))[-g] # non-self classes
+      ptrue[clinds]  <- probs[clinds, g]
+      palt[clinds]   <- apply(probs[clinds, others, drop  = FALSE], 1, max)
+      altint[clinds] <- others[apply(probs[clinds, others, drop = FALSE],
+                                     1, which.max)]
+    }
+    PAC[indsv] <- palt[indsv] / (ptrue[indsv] + palt[indsv])
+    # (PAC and altint stay NA outside indsv)
+  }
+  #
+  # Compute farness:
+  #
+  soft.shrink <-function(delta, threshold) {
+    dif <- abs(delta) - threshold
+    delta <- sign(delta) * dif * (dif > 0)
+    nonzero <- sum(drop((dif > 0) %*% rep(1, ncol(delta))) > 0)
+    attr(delta, "nonzero") <- nonzero
+    delta
+  }
+
+  diag.disc.original <-function(x, centroids, prior, weight) {
+    ### Computes the class discriminant functions assuming scaled x and centroids
+    if(! missing(weight)) {
+      posid <- (weight > 0)
+      if(any(posid)) {
+        weight <- sqrt(weight[posid])
+        centroids <- centroids[posid,  , drop = FALSE] * weight
+        x <- x[posid,  , drop = FALSE] * weight
+      }
+      else {
+        mat <- outer(rep(1, ncol(x)), log(prior), "*")
+        dimnames(mat) <- list(NULL, dimnames(centroids)[[2]])
+        return(mat)
+      }
+    }
+    dd <- t(x) %*% centroids
+    dd0 <- drop(rep(1, nrow(centroids)) %*% (centroids^2))/2 - log(prior)
+    names(dd0) <- NULL
+    scale(dd, dd0, FALSE)
+  }
+
+  mdS2 <-function(x, centroids, prior, weight) {
+    ### Computes the class discriminant functions assuming scaled x and centroids
+    if(! missing(weight)) {
+      posid <- (weight > 0)
+      if(any(posid)) {
+        weight <- sqrt(weight[posid])
+        centroids <- centroids[posid,  , drop = FALSE] * weight
+        x <- x[posid,  , drop = FALSE] * weight
+      }
+      else {
+        mat <- outer(rep(1, ncol(x)), log(prior), "*")
+        dimnames(mat) <- list(NULL, dimnames(centroids)[[2]])
+        return(mat)
+      }
+    }
+    p=ncol(t(x))
+    n=nrow(t(x))
+    k=ncol(centroids)
+    dd=matrix(NA, nrow=n, ncol=k)
+    for (k in 1:ncol(centroids)){
+      dd[,k]=mahalanobis(t(x),centroids[,k],cov=diag(p))
+    }
+    dd
+  }
+  ###################
+
+  #actually reconstrunctanting dd
+  #getting quantities needed from pamrfit
+  pamrfit=vcr.pamr.train.out$pamrfit
+
+  if (!is.null(vcr.pamr.train.out$pamrfit)){
+    stop("The vcr.pamr.train.out feeded is calculated on pamr.cv object and not on pamr.train object")
+  }
+
+
+  centroids=pamrfit$centroids #centroids per variable per class
+  centroid.overall=pamrfit$centroid.overall
+  sd=pamrfit$sd
+  threshold=pamrfit$threshold[ii]
+  se.scale=pamrfit$se.scale
+  threshold.scale=pamrfit$threshold.scale
+  prior=pamrfit$prior
+  nonzero=pamrfit$nonzero[ii]
+  K=length(prior)
+
+  #getting deltas (dik)
+  delta <- (centroids - centroid.overall)/sd
+  delta <- scale(delta, FALSE, threshold.scale * se.scale) #gives division by mk
+  #getting the shrunken ones (d'ik)
+  delta.shrunk=soft.shrink(delta,threshold) #we have a problem here, all zero
+  #getting d'ik*mk
+  delta.shrunk <- scale(delta.shrunk, FALSE, 1/(threshold.scale * se.scale))
+  nonzero_check <- attr(delta.shrunk, "nonzero")
+  if(!nonzero_check==nonzero){
+    stop(nonzero_check)
+  }
+  posid <- drop(abs(delta.shrunk) %*% rep(1, K)) > 0
+  newDistToClass <- mdS2((newx - centroid.overall)/sd, delta.shrunk, prior, weight = posid) #since the fucntion assumes scaled x, xnew is scale before
+
+
+  if (any(is.na(dd))) { ##CHECK PUT FOR DEVELOPING REASONS PROBABLY COULD REMOVE
+    stop(dd)
+  }
+
+  newDistToclass=-newDistToClass #minus because wrt to paper is with opposite sign here
+
+  rd=pamrfit$nonzero[ii] #getting reducted dimension
+
+  farout <- compFarness(type = "affine", testdata = TRUE, yint = yint,
+                        nlab = nlab, X = NULL, fig = newDistToclass,
+                        d = NULL, figparams = vcr.pamr.train.out$figparams) #DON'T REALLYU KNOW IF TO FEED OR NOT D
+
+
+
+  return(list(yintnew = yintnew,
+              ynew = levels[yintnew],
+              levels = levels,
+              predint = predint,
+              pred = levels[predint],
+              altint = altint,
+              altlab = levels[altint],
+              PAC = PAC,
+              fig = farout$fig,
+              farness = farout$farness,
+              ofarness = farout$ofarness))
+}
+
+
